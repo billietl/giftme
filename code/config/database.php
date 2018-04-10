61915d50c25913b48d8c72c5d2f530e450521c04
Louis Billiet <louis.billiet@alterway.fr>
2018-04-10 11:34:07 +0200

    use secrets

--- code/config/database.php
+++ code/config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => trim(file_get_contents('/run/secrets/db_user_password')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
